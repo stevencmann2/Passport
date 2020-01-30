43a03f8d17c1bbebbb183eeb49a238e7ffc28a57
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2020-01-30 00:06:15 -0600

    Cleaned up user dash by adding 2 buttons for view trips and create trip, however the create trip button is not working. The button populates the form due to appending form to utm.js however the form submit button does not take the form info to the my trip table.

--- public/assets/js/utm.js
+++ public/assets/js/utm.js
@@ -1,29 +1,172 @@
 //// UTM - USER TRIP MANAGER
 $(document).ready(function(){
 
-//from trip creation form
-//////////////////////////////////////// MAY WANT TO ADD TO LOWERCASE AND PARSING HERE
-const totalBudget = $("#totalbudget");
-const returning = $("#returning");
-const departing = $("#departing");
-const destination =$("#destination");
-const tripName = $("#tripname");
+$("#createTrip").show();
+
+$("#viewTrip").on("click", function () {
+  event.preventDefault();
+  window.location.href="/myTrips"
+});
+
+$("#createTrip").on("click", function () {
+  event.preventDefault();
+  $("#createTrip").hide();
+  $("#shiner").append(` <div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="page-header">
+                <h1>Where do you want to go?</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br>
+
+<div class="container">
+    <form id="user-trip-form" method="POST">
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="Input1">Trip Name</label>
+                    <input name="trip-name" type="text" class="form-control" id="tripname" required>
+                </div>
+                <div class="col-3"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="Input2">Destination</label>
+                    <input name="destination" type="text" class="form-control" id="destination" required>
+                </div>
+                <div class="col-3"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="Input3">Departing</label>
+                    <input name="departing" type="date" class="form-control" id="departing" value="2020-02-02"required>
+                </div>
+                <div class="col-3"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="Input4">Returning</label>
+                    <input name="returning" type="date" class="form-control" id="returning" value="2020-02-02" required>
+                </div>
+                <div class="col-3"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="Input4">Max Budget (dollar amount)</label>
+                    <input name="max-budget" type="number" class="form-control" id="totalbudget" required>
+                </div>
+                <div class="col-3"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <div class="form-group">
+                    <input type="submit" class="form-control submit" value="Submit">
+                </div>
+                <div class="col-3"></div>
+            </div>
+        </div>
+    </form>
+</div>`)
+});
+
+
+
+
+
+// const userTripForm = $("#user-trip-form");
+
+// // console.log(req.body)
+// // console.log(req.user)
+// ///////submits to constructors to database
+// $(userTripForm).on("submit", userTripSubmit);
+
+
+// ///// HANDLES FORM SUBMIT CLICK
+// function userTripSubmit(){
+   
+//   console.log('this is in the form')
+ 
+// // TRIP CONSTRUCTOR POST TO DB
+// // MAKE SURE PROPERTY NAMES ARE RIGHT
+// //from trip creation form
+// //////////////////////////////////////// MAY WANT TO ADD TO LOWERCASE AND PARSING HERE
+// const totalBudget = $("#totalbudget");
+// const returning = $("#returning");
+// const departing = $("#departing");
+// const destination =$("#destination");
+// const tripName = $("#tripname");
+// // event.stopImmediatePropagation();
+// event.preventDefault();
+
+
+// ///// FORMATED NOW AS yyyy-MM-dd
+// let newTrip = {
+//     tripname: tripName.val().trim(),
+//     totalbudget: parseInt(totalBudget.val().trim()),
+//     destination: destination.val().trim(),
+//     departing: departing.val(),
+//     returning: returning.val()
+
+// }
+
+// console.log(newTrip.totalbudget, typeof newTrip.totalbudget)
+// console.log(newTrip.destination, typeof newTrip.destination)
+// console.log(newTrip.departing, typeof newTrip.departing)
+// console.log(newTrip.returning, typeof newTrip.returning)
+// console.log(newTrip.tripname, typeof newTrip.tripname)
+// console.log(newTrip, typeof newTrip)
+
+// // Send the POST request.
+
+// $.ajax("/api/trips", {
+  
+//   type: "POST",
+//   data: newTrip
+  
+// }).then(
+//   function() {
+//     console.log("added new trip");
+//     // Reload the page to get the updated list
+//     window.location.href = "/myTrips";
+//   }
+// );
+// }
+
+
 const userTripForm = $("#user-trip-form");
-
-
-
 // console.log(req.body)
 // console.log(req.user)
 ///////submits to constructors to database
 $(userTripForm).on("submit", userTripSubmit);
-
-
 ///// HANDLES FORM SUBMIT CLICK
 function userTripSubmit(event){
+  const totalBudget = $("#totalbudget");
+const returning = $("#returning");
+const departing = $("#departing");
+const destination =$("#destination");
+const tripName = $("#tripname");
     event.preventDefault();
 // TRIP CONSTRUCTOR POST TO DB
 // MAKE SURE PROPERTY NAMES ARE RIGHT
-
 ///// FORMATED NOW AS yyyy-MM-dd
 let newTrip = {
     tripname: tripName.val().trim(),
@@ -31,23 +174,17 @@ let newTrip = {
     destination: destination.val().trim(),
     departing: departing.val(),
     returning: returning.val()
-
 }
-
 console.log(newTrip.totalbudget, typeof newTrip.totalbudget)
 console.log(newTrip.destination, typeof newTrip.destination)
 console.log(newTrip.departing, typeof newTrip.departing)
 console.log(newTrip.returning, typeof newTrip.returning)
 console.log(newTrip.tripname, typeof newTrip.tripname)
 console.log(newTrip, typeof newTrip)
-
 // Send the POST request.
-
 $.ajax("/api/trips", {
-  
   type: "POST",
   data: newTrip
-  
 }).then(
   function() {
     console.log("added new trip");
