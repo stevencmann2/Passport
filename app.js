02ab5e9933961180de566759cc5a7ed3ab617196
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2020-01-30 19:26:02 -0600

    Changed env file; testing heroku deployment

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ app.use(function (err, req, res, next) {
   });
 });
 
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
